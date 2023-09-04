e1273eaafb4bbaac346cfbc8e10b42e7ca759e69
Leonid <walya7534@yandex.ru>
2023-09-04 19:33:40 +0700

    refactor: set Default Locale(ru-RU) for tests

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,8 +3,15 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Locale;
+
 @SpringBootTest
 class ShareItTests {
+
+    public ShareItTests(){
+        Locale locale = Locale.forLanguageTag("ru-RU");
+        Locale.setDefault(locale);
+    }
 
     @Test
     void contextLoads() {
